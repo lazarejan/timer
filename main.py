5853390a3f6a1fd0e7b56d9897679b8eef515e97
lazare janiashvili <lazarejaniashvili9@gmail.com>
2025-02-20 20:41:10 +0400

    added tick and start_timer which runs the timer corretly

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 from tkinter import *
+from datetime import datetime
+
+temp = 0
+after_id = None
+
+def tick():
+    global temp, after_id
+    f_temp = datetime.fromtimestamp(temp).strftime("%M:%S")
+    
+    label1.configure(text = str(f_temp))
+    temp += 1
+
+    
+
+    after_id = window.after(1000, tick)
+
+def start_timer():
+    if after_id:
+        return None
+    tick()
+
+
 
 window = Tk()
 
@@ -7,7 +29,7 @@ window.title('timer')
 label1 = Label(window, width= 5, font = ('Verdana', 80), text = "00:00")
 label1.grid(row = 0, columnspan = 2)
 
-start_btn = Button(window, text = "Start", font = ('Verdana', 20))
+start_btn = Button(window, text = "Start", font = ('Verdana', 20), command = start_timer)
 start_btn.grid(row = 1, column = 0, sticky = 'ew')
 
 stop_btn = Button(window, text = "Stop", font = ('Verdana', 20))
